9086d944c4f25616e3d0d0744fc75a45349eb862
Shift <shift@laravelshift.com>
2024-06-07 23:03:15 +0000

    Adopt anonymous migrations

--- database/migrations/2021_04_16_164133_create_monthly_messages_table.php
+++ database/migrations/2021_04_16_164133_create_monthly_messages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMonthlyMessagesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -29,4 +29,4 @@ class CreateMonthlyMessagesTable extends Migration
     {
         Schema::dropIfExists('monthly_messages');
     }
-}
+};
--- database/migrations/2021_04_20_165728_create_usage__policies_table.php
+++ database/migrations/2021_04_20_165728_create_usage__policies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsagePoliciesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -29,4 +29,4 @@ class CreateUsagePoliciesTable extends Migration
     {
         Schema::dropIfExists('usage__policies');
     }
-}
+};
